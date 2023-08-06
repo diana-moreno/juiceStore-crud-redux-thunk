740ded2bb532694d4b2d6a443a63fb1a4eeed761
Diana <diana.moreno@interficie.com>
2023-08-06 13:06:45 +0200

    Updates

---
+++ precache-manifest.f665f3b336293bb0a475d1924b6a4dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b720bb6a1dc5ddfe730b3bc904a55522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68c94fd5bde0a5b60516",
+    "url": "/static/css/main.435e5ce2.chunk.css"
+  },
+  {
+    "revision": "9f7f35936ef7249570db",
+    "url": "/static/js/2.999db6a0.chunk.js"
+  },
+  {
+    "revision": "40c8bcc74f5609c62ddecaa7369b6f00",
+    "url": "/static/js/2.999db6a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68c94fd5bde0a5b60516",
+    "url": "/static/js/main.3ac28660.chunk.js"
+  },
+  {
+    "revision": "7ba86c807745e92be21f",
+    "url": "/static/js/runtime-main.ebb3302c.js"
+  },
+  {
+    "revision": "11a2189e69c59d2973a9dbfee3b000a4",
+    "url": "/static/media/pineapple-2588302_1920.11a2189e.jpg"
+  }
+]);
